5d1bea8398a152132f5a67c9a057731675b3b2b5
Quan Dang <danganhquan246@gmail.com>
2024-05-03 17:18:29 +0700

    Assertions, Exceptions and Errors

---
+++ Assertions, Exceptions and Errors/test_assert_raises.py
@@ -0,0 +1,17 @@
+def divide(numerator, denominator):
+    if denominator == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return numerator / denominator
+
+import slash
+
+with slash.assert_raises(ZeroDivisionError) as caught:          # slash.assert_raises()
+    divide(5, 0)
+
+assert str(caught.exception) == "Cannot divide by zero"         
+
+
+with slash.allowing_exceptions(ZeroDivisionError) as caught:    # slash.assert_raises()
+    pass
+
+slash.assert_almost_equal(1.001, 1, delta=0.1)                  # slash.allowing_exceptions()
